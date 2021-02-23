eb2ad4e6c69e8edcd81b5e82d15c9affffa2fba0
Josh Wymer <josh.wymer@mixpanel.com>
2021-02-22 19:34:04 -0600

    add mixpanel stuff

--- hnclone/context_processors.py
+++ hnclone/context_processors.py
@@ -3,5 +3,9 @@ def settings_context_processor(request):
    return {
        'SITE_NAME': settings.SITE_NAME,
        'SITE_DOMAIN': settings.SITE_DOMAIN,
-       'SITE_URL': settings.SITE_URL
+       'SITE_URL': settings.SITE_URL,
+       'MIXPANEL_PROJECT_TOKEN': settings.MIXPANEL_PROJECT_TOKEN,
+       'MIXPANEL_PROJECT_ID': settings.MIXPANEL_PROJECT_ID,
+       'MIXPANEL_ADMIN_USERNAME': settings.MIXPANEL_ADMIN_USERNAME,
+       'MIXPANEL_ADMIN_PASSWORD': settings.MIXPANEL_ADMIN_PASSWORD,
    }
